c1277c51d2fea781b0cbfa857979e2daa9911975
Igor Karpovich <igor@karpovich.me>
2014-08-03 11:14:36 +1100

    Added commithandler, wrote an instruction

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 bxGitlab
 ========
 
-Web-hooks for Gitlab to integrate with Bitrix CMS
+Web-hooks for Gitlab to integrate with Bitrix24 Self-hosted
+
+How to use
+==========
+
+1. You need to have both working Gitlab and Bitrix24.
+2. Put "hooks" folder anywhere within document root of your Bitrix24.
+3. Create custom user field named UF_GITLAB_ID in Bitrix24. Fill in an ID for each user.
+4. Choose WebHooks in a project settings in Gitlab, and fill in an URL for commithandler.php
+5. Enjoy :)
---
+++ hooks/commithandler.php
@@ -0,0 +1,55 @@
+<?
+define("NOT_CHECK_PERMISSIONS", true);
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+
+$handle = fopen("php://input", "rb");
+while (!feof($handle)) {
+	$http_raw_post_data .= fread($handle, 8192);
+}
+
+fclose($handlle);
+
+$arData=json_decode($http_raw_post_data, true);
+
+$rsUser=CUser::GetList(($by="ID"), ($order="DESC"), array("UF_GITLAB_ID"=>$arData["user_id"]));
+$arUser=$rsUser->Fetch();
+if(!$arUser)
+	return;
+
+$GLOBALS["USER"]->Authorize($arUser["ID"]);
+
+$TASK_ID=false;
+if(preg_match("/task([0-9]+)/i", $arData["repository"]["name"], $r))
+	$TASK_ID=$r[1];
+elseif(preg_match("/task([0-9]+)/i", $arData["repository"]["description"], $r))
+	$TASK_ID=$r[1];
+elseif(preg_match("/task([0-9]+)/i", $arData["ref"], $r))
+	$TASK_ID=$r[1];
+
+CModule::IncludeModule("tasks");
+CModule::IncludeModule("forum");
+
+$branch=$arData["ref"];
+$branch=str_replace("refs/heads/", "", $branch);
+
+foreach($arData["commits"] as $arCommit)
+{
+	$message=$arCommit["message"];
+	$message=utf8win1251($message);
+    if(preg_match("/task([0-9]+)/i", $message, $r))
+		$TASK_ID=$r[1];
+	if(!$TASK_ID)
+		continue;
+	$message=str_replace($r[0], "", $message);
+	
+	$rsTask=CTasks::GetList(array(), array("ID"=>$TASK_ID));
+	$arTask=$rsTask->Fetch();
+	if(!$arTask)
+		continue;
+	
+	CTaskComments::add($arTask["ID"], $arUser["ID"],
+        "<b>Commit:</b> <a href=".$arCommit["url"].">".substr($arCommit["id"], 0, 9)."</a>\nBranch: ".$branch."\n".$message);
+}
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/epilog_after.php");
+?>
